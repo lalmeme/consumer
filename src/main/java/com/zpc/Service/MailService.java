9becd43f9ad3d85be3b466fb879757010e86d7e9
zpc <zpc@finereport.com>
2016-12-28 23:27:59 +0800

    activemq与spring整合
    实现异步发送邮件的功能，该工程为消费者

---
+++ src/main/java/com/zpc/Service/MailService.java
@@ -0,0 +1,45 @@
+package com.zpc.Service;
+
+import com.zpc.entity.MailEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ExceptionDepthComparator;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * Created by zpc on 16/12/28.
+ */
+@Service("mailService")
+public class MailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    private SimpleMailMessage simpleMailMessage;
+
+    @Autowired
+    private ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+    public void mailSend(final MailEntity mailEntity) {
+        threadPoolTaskExecutor.execute(new Runnable() {
+            public void run() {
+                try {
+                    simpleMailMessage.setFrom(mailEntity.getFrom());
+                    simpleMailMessage.setTo(mailEntity.getTo());
+                    simpleMailMessage.setSubject(mailEntity.getSubject());
+                    simpleMailMessage.setText(mailEntity.getContent());
+
+                    javaMailSender.send(simpleMailMessage);
+                } catch (MailException e) {
+                    e.printStackTrace();
+                    throw e;
+                }
+            }
+        });
+    }
+}
---
+++ src/main/java/com/zpc/mq/MailQueueMassageListener.java
@@ -0,0 +1,61 @@
+package com.zpc.mq;
+
+import com.alibaba.fastjson.JSONObject;
+import com.zpc.Service.MailService;
+import com.zpc.entity.MailEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.listener.SessionAwareMessageListener;
+import org.springframework.stereotype.Component;
+
+import javax.jms.*;
+
+/**
+ * Created by zpc on 16/12/28.
+ */
+@Component
+public class MailQueueMassageListener implements SessionAwareMessageListener<Message> {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    private Destination destination;
+
+    @Autowired
+    private MailService mailService;
+
+
+    public synchronized void onMessage(Message message, Session session) throws JMSException {
+        try {
+            TextMessage textMessage = (TextMessage) message;
+
+            final String ms = textMessage.getText();
+
+            System.out.println("收到消息：" + ms);
+            //转换成相应的对象
+
+            MailEntity mail = JSONObject.parseObject(ms, MailEntity.class);
+
+            if (mail == null) {
+                return;
+            }
+
+            try {
+                //执行发送邮件
+                mailService.mailSend(mail);
+            } catch (Exception e) {
+                // 发送异常，重新放回队列
+//				jmsTemplate.send(mailQueue, new MessageCreator() {
+//					@Override
+//					public Message createMessage(Session session) throws JMSException {
+//						return session.createTextMessage(ms);
+//					}
+//				});
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
